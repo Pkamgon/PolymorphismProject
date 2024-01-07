d5a3cc1ee6ed8afae89afb8f02e52d2e65dc012f
Pkamgon <Pkamgon@gmail.com>
2024-01-07 08:25:09 -0700

    Add project files.

---
+++ PolymorphismProject/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PolymorphismProject
+{
+    class Employee : IQuittable
+    {
+        public string FirstName { get; set;}
+        public string LastName { get; set; }
+
+        internal Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void Quit()
+        {
+
+            Console.WriteLine("Employee Quit");
+            Console.ReadLine();
+        }
+    }      
+}
---
+++ PolymorphismProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          //Create an Employee object
+           Employee employee = new Employee("Peter", "Paul");
+            
+           IQuittable quittableEmployee = employee;
+
+           quittableEmployee.Quit();
+        }
+    }
+}
